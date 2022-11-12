cd979834fecca1bd5d53ac145cf6dd2acf6a71b1
Adrian Evangelista <adriane.macer@gmail.com>
2022-11-13 07:58:01 +0800

    Added contents for games, more and profile pages

--- lib/presentation/screens/game/game_screen.dart
+++ lib/presentation/screens/game/game_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:textless/textless.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: "Game Screen".text,
-        ));
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Image.asset(
+          "assets/images/games.png",
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/screens/more/more_screen.dart
+++ lib/presentation/screens/more/more_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:textless/textless.dart';
 
 class MoreScreen extends StatelessWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: "More Screen".text,
-        ));
+    return Image.asset(
+      "assets/images/more.png",
+      fit: BoxFit.fitWidth,
+    );
   }
 }
